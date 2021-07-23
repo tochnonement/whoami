33c30dd98e8ff529651cb9ca89c73adfefe2747c
Aleksandrs Filipovskis <tochnonement@gmail.com>
2021-07-23 23:03:30 +0300

    Name above player's head

--- gamemode/core/cl_hooks.lua
+++ gamemode/core/cl_hooks.lua
@@ -26,6 +26,18 @@ function GM:HUDPaint()
     end
 end
 
+function GM:PostPlayerDraw(ply)
+    local eyeAng = EyeAngles()
+    local boneID = ply:LookupBone("ValveBiped.Bip01_Head1")
+    local boneMatrix = ply:GetBoneMatrix(boneID)
+    local pos = boneMatrix:GetTranslation() + Vector(0, 0, 16)
+    local ang = Angle(0, eyeAng.y - 90, 90)
+
+    cam.Start3D2D(pos, ang, 0.15)
+        draw.SimpleText(ply:Name(), "DermaLarge", 0, 0, color_white, 1, 1)
+    cam.End3D2D()
+end
+
 function GM:HUDShouldDraw(name)
     if hide[name] then
         return false
