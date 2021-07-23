dd65b41275aaee0a566d724516c6ec0080f7270f
Aleksandrs Filipovskis <tochnonement@gmail.com>
2021-07-24 02:23:28 +0300

    New word to guess

---
+++ gamemode/config/packs/creatures.lua
@@ -0,0 +1,40 @@
+--[[
+
+Author: tochonement
+Email: tochonement@gmail.com
+
+24.07.2021
+
+--]]
+
+local CATEGORY = "Creatures"
+
+whoi.word.register("goblin")
+:SetCategory(CATEGORY)
+:SetName("Goblin")
+:SetImage("https://i.imgur.com/7GIIezt.png")
+
+whoi.word.register("dragon")
+:SetCategory(CATEGORY)
+:SetName("Dragon")
+:SetImage("https://i.imgur.com/Gbh2ryN.png")
+
+whoi.word.register("dwarf")
+:SetCategory(CATEGORY)
+:SetName("Dwarf")
+:SetImage("https://i.imgur.com/XwYBMcj.png")
+
+whoi.word.register("cyclops")
+:SetCategory(CATEGORY)
+:SetName("Cyclops")
+:SetImage("https://i.imgur.com/biAyhG4.png")
+
+whoi.word.register("vampire")
+:SetCategory(CATEGORY)
+:SetName("Vampire")
+:SetImage("https://i.imgur.com/AU0Qkqb.png")
+
+whoi.word.register("zombie")
+:SetCategory(CATEGORY)
+:SetName("Zombie")
+:SetImage("https://i.imgur.com/7gfchU3.png")
---
+++ gamemode/config/packs/fruits.lua
@@ -0,0 +1,30 @@
+--[[
+
+Author: tochonement
+Email: tochonement@gmail.com
+
+24.07.2021
+
+--]]
+
+local CATEGORY = "Fruits"
+
+whoi.word.register("banana")
+:SetCategory(CATEGORY)
+:SetName("Banana")
+:SetImage("https://i.imgur.com/5YlW1dt.png")
+
+whoi.word.register("apple")
+:SetCategory(CATEGORY)
+:SetName("Apple")
+:SetImage("https://i.imgur.com/tCpygPE.png")
+
+whoi.word.register("orange")
+:SetCategory(CATEGORY)
+:SetName("Orange")
+:SetImage("https://i.imgur.com/b6Eqk6r.png")
+
+whoi.word.register("watermelon")
+:SetCategory(CATEGORY)
+:SetName("Watermelon")
+:SetImage("https://i.imgur.com/8jZxN4f.png")
