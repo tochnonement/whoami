8e2ee7b8eceb7ace0305b4f4a967314ff93cd41c
Aleksandrs Filipovskis <tochnonement@gmail.com>
2021-07-24 16:54:32 +0300

    Fix for network message

--- gamemode/core/sv_net.lua
+++ gamemode/core/sv_net.lua
@@ -11,7 +11,10 @@ netez.register("SelectWord")
 :AddField("string")
 :SetDelay(1)
 :SetCallback(function(ply, wordId)
-    if (whoi.round.getWisher() == ply) and whoi.word.get(wordId) then
+    local word = whoi.word.get(wordId)
+
+    if (whoi.round.getState() == whoi.state.PREPARING) and (whoi.round.getWisher() == ply) and word then
         whoi.round.selectWord(wordId)
+        whoi.util.print(ply:Name() .. " selected word: " .. word:GetName())
     end
 end)
